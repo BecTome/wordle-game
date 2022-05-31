c4dba3f02426f7e3c23432a0649fcb37a49c24db
Alberto Becerra <alberto.becerra.tome@gmail.com>
2022-05-31 16:57:41 +0200

    Descripciones de parámetros

--- config.py
+++ config.py
@@ -2,6 +2,11 @@
 import sys
 sys.path.append(".")
 
+# Path to our words list
 DICTPATH = 'data\\diccionario-sin-tildes.txt'
+
+# Maximum Trials Allowed
 N_TRIES = 6
+
+# Max Length Allowed for Words
 MAX_WORD_LEN = 5
